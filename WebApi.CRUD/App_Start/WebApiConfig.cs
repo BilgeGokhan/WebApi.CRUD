5e33c1d4970ca1ee0f1b06575babade27b05cf3b
BilgeGokhan <47634641+BilgeGokhan@users.noreply.github.com>
2020-01-30 01:39:11 +0300

    CRUD with WebApi

---
+++ WebApi.CRUD/App_Start/WebApiConfig.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace WebApi.CRUD
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            /* Döngüsel REferans Yönetimi - Reference Loop Handling -- EF içerisinde birbirlerine bağlı tabloların loop a girmemesi için yapılabilecek işlemler .
+             * 
+             * 
+                config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                config.Formatters.JsonFormatter.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.All;
+
+                3. bir yöntem ise Propery seviyesinde yapmak ilgili nesneye gidip Attribute ( [JsonIgnore] ) ile engelleme yapılabilir. 
+            Örnek:
+                public partial class Product
+                {
+                    public int ProductId { get; set; }
+
+                    [JsonIgnore]
+                    [IgnoreDataMember] Xml için
+                    public virtual Category Category { get; set; }
+                }    
+                
+                Referans ı korumak için ise korunacak class ın tepesine [JsonObject(IsReference = true)] yazıyoruz. Fakat bu çıktıda $ işaretli Json formatına uymayan serileştirme işlemini handle etmek gerekiyor. 
+
+            */
+
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+            
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ WebApi.CRUD/Controllers/EmployeeController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi.CRUD.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        ExampleEntities db = new ExampleEntities();
+
+        public HttpResponseMessage Get(string gender = "All", int? top = 0)
+        {
+            IQueryable<Employee> query = db.Employees;
+
+            gender = gender.ToLower();
+
+            switch (gender)
+            {
+                case "all":
+                    break;
+                case "erkek":
+                case "kadın":
+                    query = query.Where(q => q.Gender.ToLower() == gender);
+                    break;
+                default:
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, $"{gender} bulunamadı. Lütfen All, Erkek yada Kadın parametrelerini kullanın.");
+            }
+
+            if (top > 0)
+            {
+                query = query.Take(top.Value);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, query.ToList());
+        }
+
+        public HttpResponseMessage Get(int id)
+        {
+            Employee emp = db.Employees.FirstOrDefault(x => x.Id == id);
+
+            if (emp == null)
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, $"Id'si {id} olan çalışan bulunamadı.");
+            else
+                return Request.CreateResponse(HttpStatusCode.OK, emp);
+        }
+
+        public HttpResponseMessage Post(Employee emp)
+        {
+            try
+            {
+                db.Employees.Add(emp);
+
+                if (db.SaveChanges() > 0)
+                {
+                    HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, emp);
+                    response.Headers.Location = new Uri(Request.RequestUri + "/" + emp.Id);
+
+                    return response;
+                }
+                else
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Veri ekleme işlemi yapılamadı.");
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        public HttpResponseMessage Put(Employee emp)
+        {
+            try
+            {
+                Employee e = db.Employees.FirstOrDefault(x => x.Id == emp.Id);
+
+                if (e == null)
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, $"Employee Id: {emp.Id}");
+                else
+                {
+                    e.Name = emp.Name;
+                    e.Surname = emp.Surname;
+                    e.Salary = emp.Salary;
+                    e.Gender = emp.Gender;
+
+                    if (db.SaveChanges() > 0)
+                    {
+                        return Request.CreateResponse(HttpStatusCode.OK, emp);
+                    }
+                    else
+                        return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Güncelleme yapılamadı.");
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        //FromUri, FromBody Örneği -- İki FormBody yapmak için ComplexType kullanmalıyız. 
+        public HttpResponseMessage Put([FromUri]int id, [FromBody]Employee emp)
+        {
+            try
+            {
+                Employee e = db.Employees.FirstOrDefault(x => x.Id == id);
+
+                if (e == null)
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, $"Employee Id: {id}");
+                else
+                {
+                    e.Name = emp.Name;
+                    e.Surname = emp.Surname;
+                    e.Salary = emp.Salary;
+                    e.Gender = emp.Gender;
+
+                    if (db.SaveChanges() > 0)
+                    {
+                        return Request.CreateResponse(HttpStatusCode.OK, emp);
+                    }
+                    else
+                        return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Güncelleme yapılamadı.");
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        public HttpResponseMessage Delete(int id)
+        {
+            try
+            {
+                var e = db.Employees.FirstOrDefault(x => x.Id == id);
+
+                if (e == null)
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, $"Employee Id: {id}");
+                else
+                {
+                    db.Employees.Remove(e);
+
+                    if (db.SaveChanges() > 0)
+                    {
+                        return Request.CreateResponse(HttpStatusCode.OK, $"Employee Id: {id}");
+                    }
+                    else
+                        return Request.CreateErrorResponse(HttpStatusCode.BadRequest, $"Kayıt Silinemedi. Employee ID: {id}");
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+    }
+}
